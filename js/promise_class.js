378b0411de99c19b0a84c7aac1d7c6ce1a7b4f8b
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-29 12:13:29 +0800

    更新

--- js/promise_class.js
+++ js/promise_class.js
@@ -12,38 +12,36 @@
       self.status = PENDING //实例一开始是初始化状态
       self.data = undefined //实例一开始数据为空
       self.callbacks = [] //实例对应的一组一组的回调们，形如：[{onResolved：函数，onRejected：函数},...]
+      //定义resolve函数，调用后：1.状态切为resolved; 2.存储成功的value; 3.依次执行该实例所对应的每一个onResolved
+      function resolve(value) {
+        if(self.status !== PENDING) return
+        //1.状态切为resolved;
+        self.status = RESOLVED
+        //2.存储成功的value;
+        self.data = value
+        //3.依次执行回调
+        setTimeout(()=>{
+          self.callbacks.forEach((callbackObj)=>{
+            callbackObj.onResolved(self.data)
+          })
+        })
+      }
+      //定义reject函数,调用后：1.状态切为rejected; 2.存储失败的reason; 3.执行onRejected
+      function reject(reason) {
+        if(self.status !== PENDING) return
+        //1.状态切为rejected;
+        self.status = REJECTED
+        //2.存储成功的value;
+        self.data = reason
+        //3.依次执行回调
+        setTimeout(()=>{
+          self.callbacks.forEach((callbackObj)=>{
+            callbackObj.onRejected(self.data)
+          })
+        })
+      }
       //调用程序员传过来的executor
-      executor(self.resolve,self.reject)
-    }
-
-    //定义resolve函数，调用后：1.状态切为resolved; 2.存储成功的value; 3.依次执行该实例所对应的每一个onResolved
-    resolve(value) {
-      if(self.status !== PENDING) return
-      //1.状态切为resolved;
-      self.status = RESOLVED
-      //2.存储成功的value;
-      self.data = value
-      //3.依次执行回调
-      setTimeout(()=>{
-        self.callbacks.forEach((callbackObj)=>{
-          callbackObj.onResolved(self.data)
-        })
-      })
-    }
-
-    //定义reject函数,调用后：1.状态切为rejected; 2.存储失败的reason; 3.执行onRejected
-    reject(reason) {
-      if(self.status !== PENDING) return
-      //1.状态切为rejected;
-      self.status = REJECTED
-      //2.存储成功的value;
-      self.data = reason
-      //3.依次执行回调
-      setTimeout(()=>{
-        self.callbacks.forEach((callbackObj)=>{
-          callbackObj.onRejected(self.data)
-        })
-      })
+      executor(resolve,reject)
     }
 
     //Promise原型对象上的then
